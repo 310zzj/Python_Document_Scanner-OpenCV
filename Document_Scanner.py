c766fb8995cb014d51bbd5751e85d5eaffed63b8
310zzj <310zzj@proton.me>
2021-02-28 15:13:32 +0000

    dm persistent data: fix allocation failure in space map checker init

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -8,4 +8,6 @@ print("Imports are Done!")
 
 class Scanner:
 	"""
-	A class to scan (and perform related operations on) a documen
+	A class to scan (and perform related operations on) a document.
+
+	..
