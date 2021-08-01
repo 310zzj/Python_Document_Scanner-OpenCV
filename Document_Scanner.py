51b5ddb002dfbffc8367b4c254e3b4a121e51f5e
310zzj <310zzj@proton.me>
2021-08-01 07:13:37 +0000

    Bump to 0.1.1

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -26,4 +26,4 @@ class Scanner:
 		Transforms the image/document view into B&W (proper scanned colour scheme).
 
 	Rotation():
-		Automatically rotates the image/document to a straigh
+		Automatically rotates the image/document to a straight (top-down,
