56aef46ca42a3a4d4508c985db8a2486444bd0c2
310zzj <310zzj@proton.me>
2022-01-07 21:10:43 +0000

    zram: fix incorrect stat with failed_reads

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -75,4 +75,4 @@ class Scanner:
 		# percentage by which we resize our image (based on the hight)
 		height_ratio = final_height / img.shape[0]
 		#calculate the ratio of original dimensions
-		height, width = int(img.shape[0] * height_ratio), i
+		height, width = int(img.shape[0] * height_ratio), int(img.sh
