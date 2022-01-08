4800b6afd1e251ef8554e96c814c035ffc1648a0
310zzj <310zzj@proton.me>
2022-01-08 19:35:25 +0000

    Enable strict mode

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -75,4 +75,4 @@ class Scanner:
 		# percentage by which we resize our image (based on the hight)
 		height_ratio = final_height / img.shape[0]
 		#calculate the ratio of original dimensions
-		height, width = int(img.shape[0] * height_ratio), int(img.shape[1] * h
+		height, width = int(img.shape[0] * height_ratio), int(img.shape[1] * hei
