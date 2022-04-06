01417dc8635377ef2c41a382ccc3028e613c29df
310zzj <310zzj@proton.me>
2022-04-06 08:16:35 +0000

    Added preview image

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -96,4 +96,4 @@ class Scanner:
 		resize_collage : bool
 			flag to resize the collage
 		resize_height : int (default = 500)
-			final height to resize an im
+			final height to resize an image to (in
