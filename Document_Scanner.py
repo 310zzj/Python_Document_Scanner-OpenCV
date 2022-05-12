b65207be90511c3c465e5f3ea980ab31df3a71f5
310zzj <310zzj@proton.me>
2022-05-12 08:39:23 +0000

    LeeL Blog - Site Update

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -112,4 +112,5 @@ class Scanner:
 		# convert our image to grayscale, apply threshold
 		# to create scanned paper effect
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		thr = threshold_local(image, 11, offset = 10, method = "gaussian"
+		thr = threshold_local(image, 11, offset = 10, method = "gaussian")
+		imag
