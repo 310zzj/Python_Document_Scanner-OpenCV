d508266871ac2641655811d33652fdb00d4b6942
310zzj <310zzj@proton.me>
2022-06-09 10:00:14 +0000

    Indentation cleanup

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -123,4 +123,4 @@ class Scanner:
 		print(np.shape(orig), np.shape(image))
 		if save_collage:
 			# Saving a nice horizontal collage
-			horiz_conc = np.concatenate((orig[:,:,
+			horiz_conc = np.concatenate((orig[:,:,1], ima
