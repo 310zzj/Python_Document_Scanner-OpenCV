7600e61c504cad9403a020dc96d0aae728da57c6
310zzj <310zzj@proton.me>
2022-08-01 12:21:01 +0000

    Add clear button

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -136,4 +136,4 @@ class Scanner:
 
 	def Rotation(self, save_rotated=False, resize_height=500):
 		"""
-		Rotate an image/document view for a face-on view (view from t
+		Rotate an image/document view for a face-on view (view from the top).
