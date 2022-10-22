80d49fbadef65e17bc94b4b75ce233af526171db
310zzj <310zzj@proton.me>
2022-10-22 08:35:32 +0000

    usb-storage: ignore ZTE MF 823 card reader in mode 0x1225

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -163,4 +163,5 @@ class Scanner:
 		lines = cv2.HoughLinesP(img_edges, rho=1, theta=np.pi / 180.0, threshold=160, minLineLength=100, maxLineGap=10)
 		
 		# calculate all the angles:
-		angles
+		angles = []
+	
