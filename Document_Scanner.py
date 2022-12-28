223f7dea32ac8f4ad7ca07173b80be446f24cc36
310zzj <310zzj@proton.me>
2022-12-28 09:38:49 +0000

    Fixed bug.

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -176,4 +176,4 @@ class Scanner:
 		image = ndimage.rotate(image, median_angle)
 
 		# Resize the rotated image to the old height
-		#image = self.Resize_Image(resize_height ,
+		#image = self.Resize_Image(resize_height , image)
