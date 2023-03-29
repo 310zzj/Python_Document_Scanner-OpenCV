0409e02c15784c240511759dbc7b4c7c9fe468cd
310zzj <310zzj@proton.me>
2023-03-29 18:06:34 +0000

    release 1.0.4

--- Document_Scanner.py
+++ Document_Scanner.py
@@ -201,4 +201,5 @@ if __name__=="__main__":
 	#scan.Resize_Image(512, img)
 	
 	# Scanning the image -> B&W scheme
-	scanned_im = scan.Scan_View(save_collage=False, resize_collage=False, resize_height
+	scanned_im = scan.Scan_View(save_collage=False, resize_collage=False, resize_height =500)
+
